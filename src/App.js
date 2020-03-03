76e0feb3d678a1dcf50da6850f7555ef945d2557
Matthew Taylor <majikpig@gmail.com>
2020-03-02 21:31:33 -0500

    initial setup

--- src/App.js
+++ src/App.js
@@ -1,12 +1,28 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
+const apiKey = `api_key=DvijkRr9GB6oxQfL0Ch5ZmFcBIIa9aQrgH4q89yk`;
+const apiUrl = `https://api.nasa.gov/planetary/apod?`;
+
 function App() {
+  const [imageData, setImageData] = useState('');
+
+  const getImage = () => {
+    if (imageData==='') {
+      axios.get(`${apiUrl}${apiKey}`)
+      .then((response)=>setImageData(response.data))
+      .catch(error=>console.log(error));
+    }
+  };
+
+  useEffect(getImage,[]);
+
   return (
     <div className="App">
       <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun 🚀!
+        <button onClick={getImage}>Click me</button>
+        {imageData===''?'':<img src={imageData.url} alt={imageData.title} style={{width: `300px`}} />}
       </p>
     </div>
   );
